6f723d719e830da8ee147b8b73ff997d42798fd9
RandomDemon <104325602+RandomDemon@users.noreply.github.com>
2023-01-02 15:02:07 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ const { token } = require("./config.json");
 const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+
+//Init Discord.js and the commands
 var isReady = false;
-
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-
 client.commands = new Collection();
+
+// Reads the files in the 'commands' dir
 const commandsPath = path.join(__dirname, 'commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
+// Iterate over each command file, require it, and add it to the 'client.commands' Collection
 for (const file of commandFiles) {
 	const filePath = path.join(commandsPath, file);
 	const command = require(filePath);
@@ -17,9 +20,11 @@ for (const file of commandFiles) {
   console.log("loaded " + file);
 }
 
+// When the client is ready, log a message to the console (NOT in the Discord server)
 client.once(Events.ClientReady, () => {
 	console.log("Bot online");
   
+	// Log how many servers the bot is logged in to
   if (client.guilds.cache.size == 1)
   {
     console.log("Logged into 1 server");
@@ -30,6 +35,7 @@ client.once(Events.ClientReady, () => {
   isReady = true;
 });
 
+// When a chat input command is received, try to execute it
 client.on(Events.InteractionCreate, async interaction => {
 	if (!interaction.isChatInputCommand()) return;
 
@@ -45,4 +51,5 @@ client.on(Events.InteractionCreate, async interaction => {
 	}
 });
 
+// Log the bot in to the Discord API
 client.login(token);
