a241594342ecdbf4473aa66633c7ad1009a77aa8
RandomDemon <104325602+RandomDemon@users.noreply.github.com>
2023-01-02 20:21:59 -0500

    Add comments to ping command

--- commands/pingserver.js
+++ commands/pingserver.js
@@ -2,6 +2,7 @@ const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const { MinecraftServerListPing, MinecraftQuery } = require("minecraft-status");
 
 module.exports = {
+	// Command options
 	data: new SlashCommandBuilder()
 		.setName("pingserver")
 		.setDescription("Pings a server for info")
@@ -13,20 +14,26 @@ module.exports = {
 			option.setName("port")
 				.setDescription("The port of the server to ping")
         .setRequired(true)),
+	// Execute the command
 	async execute(interaction) {
+	// Ping status
     await interaction.reply("pinging...");
     
+	// Fetch IP and Port from the command
     const ip = interaction.options.getString("ip");
     const port = interaction.options.getInteger("port");
         
+	// Parse the server's description
     function getDescription(response) {
       var description = "";
       if (response.description.extra != null) {
         if (response.description.extra[0].extra == null) {
+	// concatenate the text fields of each object in the extra array
           for (var i = 0; i < response.description.extra.length; i++) {
             description += response.description.extra[i].text;
           }
         } else {
+	// concatenate the text fields of each object in the extra[0].extra array
           for (var i = 0; i < response.description.extra[0].extra.length; i++) {
             description += response.description.extra[0].extra[i].text;
           }
@@ -37,10 +44,12 @@ module.exports = {
         description = response.description.translate;
       } else if ("description: " + response.description != null) {
         description = response.description;
+	// if none of the above cases apply, set the description to a default value
       } else {
         description = "Couldn't get description";
       }
 
+	// if the description is too long, truncate it and add an ellipsis
       if (description.length > 150) {
         description = description.substring(0, 150) + "...";
       }
@@ -50,7 +59,9 @@ module.exports = {
         description = description.substring(2, description.length);
       }
 
+	// Split into an array and concatenate all elements
       if (description.split('§').length > 1) {
+	// Use § as the delimiter
         var splitDescription = description.split('§');
 
         description = splitDescription[0];
@@ -59,6 +70,7 @@ module.exports = {
         }
       }
 
+	// If desc. is empty, set a default value
       if (description == '') {
         description = 'ㅤ';
       }
@@ -68,6 +80,7 @@ module.exports = {
     
     MinecraftServerListPing.ping(0, ip, port, 2000)
       .then(response => {
+	// Parse server description
         var description = getDescription(response);
 
         var newEmbed = new EmbedBuilder()
@@ -75,6 +88,7 @@ module.exports = {
           .setTitle('Ping Results')
           .setAuthor({ name: 'MC Server Scanner', iconURL: 'https://cdn.discordapp.com/app-icons/1037250630475059211/21d5f60c4d2568eb3af4f7aec3dbdde5.png'/*, url: 'https://discord.js.org' */})
           .addFields(
+		// Add all fields to embed
             { name: 'ip', value: ip },
             { name: 'port', value: String(port) },
             { name: 'version', value: response.version.name },
@@ -85,6 +99,7 @@ module.exports = {
         
         interaction.editReply({ content:'', embeds:[newEmbed] });
       })
+	// If any errors occur, send to channel
       .catch(error => {
         interaction.editReply("ip is invalid or server is offline");
       });
