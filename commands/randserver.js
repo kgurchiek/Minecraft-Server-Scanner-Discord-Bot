162b75ad64ca4bd3aa6abb92bbc91956b37d31a1
kgurchiek <kgurchiek1@gmail.com>
2023-01-27 10:20:35 -0800

    changed ping method to a faster api

--- commands/randserver.js
+++ commands/randserver.js
@@ -67,31 +67,35 @@ module.exports = {
       //console.log(successIPs[matchNumber] + ":" + successPorts[matchNumber]);
 	    // Generate a random number to select a server from the list of successful pings
 
-      MinecraftServerListPing.ping(0, successIPs[matchNumber], successPorts[matchNumber], 1500)
+      fetch("https://api.mcstatus.io/v2/status/java/" + successIPs[matchNumber])
+      .then(text => text.json())
       .then(response => {
         //console.log(response);
 
-        var description = getDescription(response);
+        if (response.online) {
+          var newEmbed = new EmbedBuilder()
+            .setColor("#02a337")
+            .setTitle('Random Server')
+            .setAuthor({ name: 'MC Server Scanner', iconURL: 'https://cdn.discordapp.com/app-icons/1037250630475059211/21d5f60c4d2568eb3af4f7aec3dbdde5.png'/*, url: 'https://discord.js.org' */})
+            .setThumbnail("https://api.mcstatus.io/v2/icon/" + successIPs[matchNumber])
+            .addFields(
+              { name: 'ip', value: successIPs[matchNumber] },
+              { name: 'port', value: String(successPorts[matchNumber]) },
+              { name: 'version', value: response.version.name_clean },
+              { name: 'description', value: response.motd.clean },
+              { name: 'players', value: response.players.online + '/' + response.players.max }
+            )
+            .setTimestamp()
 
-        var newEmbed = new EmbedBuilder()
-          .setColor("#02a337")
-          .setTitle('Random Server')
-          .setAuthor({ name: 'MC Server Scanner', iconURL: 'https://cdn.discordapp.com/app-icons/1037250630475059211/21d5f60c4d2568eb3af4f7aec3dbdde5.png'/*, url: 'https://discord.js.org' */})
-          .setThumbnail("https://api.mcstatus.io/v2/icon/" + successIPs[matchNumber])
-          .addFields(
-            { name: 'ip', value: successIPs[matchNumber] },
-            { name: 'port', value: String(successPorts[matchNumber]) },
-            { name: 'version', value: response.version.name },
-            { name: 'description', value: description },
-            { name: 'players', value: response.players.online + '/' + response.players.max }
-          )
-          .setTimestamp()
-
-        interaction.editReply({ content:'', embeds: [newEmbed] });
+          interaction.editReply({ content:'', embeds: [newEmbed] });
+        } else {
+          // if the server is offline, try again with another server
+          sendMessage();
+        }
       })
       .catch(error => {
         //console.log(error);
-	      // If the ping request fails, try again with a different server
+	      // If the ping fails, try again with a different server
         sendMessage();
       });
     }
