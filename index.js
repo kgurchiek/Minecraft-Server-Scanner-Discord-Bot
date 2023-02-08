f1f3ded2076dda3d2fedc61397f35e48d23f8289
kgurchiek <90143063+kgurchiek@users.noreply.github.com>
2023-02-07 22:16:15 -0800

    added back index.js
    accidentally deleted in pull request

---
+++ index.js
@@ -0,0 +1,47 @@
+require("dotenv").config()
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+
+//Init Discord.js and the commands
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+client.commands = new Collection();
+
+// Reads the files in the 'commands' dir
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+// Iterate over each command file, require it, and add it to the 'client.commands' Collection
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	client.commands.set(command.data.name, command);
+  console.log("[Loaded]: " + file);
+}
+
+// When the client is ready, log a message to the console (NOT in the Discord server)
+client.once(Events.ClientReady, () => {
+  // Log how many servers the bot is logged in to
+  console.log(`[Bot]: ${client.user.tag}`)
+  console.log("[Servers]: " + client.guilds.cache.size);
+
+});
+
+// When a chat input command is received, try to execute it
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error("[Error]: " + error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
+
+// Log the bot in to the Discord API
+client.login(process.env.token);
