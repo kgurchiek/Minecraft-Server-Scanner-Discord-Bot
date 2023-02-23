ae2e795c845a1651bdeb29dba485c52bd68df2a1
RandomDemon <104325602+RandomDemon@users.noreply.github.com>
2023-02-23 13:19:35 -0500

    Updated some comments

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Partials, Collection, Events, GatewayIntentBits } = require('discord.js');
 
-//Init Discord.js and the commands
+// Initialize Discord.js (Along with the commands)
 const client = new Client({ partials: [Partials.Channel], intents: [GatewayIntentBits.Guilds, GatewayIntentBits.DirectMessages] });
 client.commands = new Collection();
 
-// Reads the files in the 'commands' dir
+// Reads the files in the commands directory
 const commandsPath = path.join(__dirname, 'commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
-// Iterate over each command file, require it, and add it to the 'client.commands' Collection
+// Iterate over each command file, require it, and add it to the 'client.commands' collection
 for (const file of commandFiles) {
   const filePath = path.join(commandsPath, file);
   const command = require(filePath);
@@ -21,13 +21,13 @@ for (const file of commandFiles) {
 
 // When the client is ready, log a message to the console (NOT in the Discord server)
 client.once(Events.ClientReady, () => {
-  // Log how many servers the bot is logged in to
+  // Logs how many servers the bot is logged in to
   console.log(`[Bot]: ${client.user.tag}`)
   console.log("[Servers]: " + client.guilds.cache.size);
 
 });
 
-// When a chat input command is received, try to execute it
+// When a chat input command is received, attempt to execute it
 client.on(Events.InteractionCreate, async interaction => {
   try {
     if (!interaction.isChatInputCommand()) return;
@@ -47,5 +47,5 @@ client.on(Events.InteractionCreate, async interaction => {
   }
 });
 
-// Log the bot in to the Discord API
+// Log the bot into the Discord API
 client.login(config.token);
