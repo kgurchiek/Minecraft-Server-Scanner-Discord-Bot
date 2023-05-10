0fc9f74cebf03c473bbfbcd347594575c8cbe767
kgurchiek <kgurchiek1@gmail.com>
2023-05-09 23:18:01 -0700

    use MongoDB Query for /random

--- commands/random.js
+++ commands/random.js
@@ -1,55 +1,46 @@
 // Imports
-const wait = require('node:timers/promises').setTimeout;
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
-const fetch = require("node-fetch");
 const { getDescription, getVersion } = require('../commonFunctions.js')
 
 module.exports = {
-  // Define 'randserver' command
+  // Define 'random' command
   data: new SlashCommandBuilder()
     .setName('random')
 	  .setDescription('Gets a random Minecraft server'),
   async execute(interaction) {
+    const { scannedServersDB } = require('../index.js');
     // Status message
     await interaction.reply("Getting a server, please wait...");
-
-    // Import server data
-    const { scannedServers } = require('../index.js');
-    if (scannedServers == null) {
-      var errorEmbed = new EmbedBuilder()
-        .setColor("#ff0000")
-        .addFields({ name: 'Error', value: 'Fetching api, try again in a few seconds.' })
-      await interaction.editReply({ embeds: [errorEmbed] })
-      return;
-    }
     
-    // Generate a random number to select a server from the list of successful pings
-    var index = Math.round((Math.random() * scannedServers.length));
+    // Get a random server from the database
+    const totalServers = await scannedServersDB.countDocuments();
+    var index = Math.floor((Math.random() * totalServers));
+    const server = (await (await scannedServersDB.find({}).skip(index).limit(1)).toArray())[0];
     var newEmbed = new EmbedBuilder()
         .setColor("#02a337")
         .setTitle('Search Results')
         .setAuthor({ name: 'MC Server Scanner', iconURL: 'https://cdn.discordapp.com/app-icons/1037250630475059211/21d5f60c4d2568eb3af4f7aec3dbdde5.png' })
-        .setThumbnail(`https://ping.cornbread2100.com/favicon/?ip=${scannedServers[index].ip}&port=${scannedServers[index].port}`)
+        .setThumbnail(`https://ping.cornbread2100.com/favicon/?ip=${server.ip}&port=${server.port}`)
         .addFields(
-          { name: 'Result ' + 1 + '/' + scannedServers.length, value: 'ㅤ' },
-          { name: 'IP', value: scannedServers[index].ip },
-          { name: 'Port', value: (scannedServers[index].port + '') },
-          { name: 'Version', value: getVersion(scannedServers[index].version) },
-          { name: 'Description', value: getDescription(scannedServers[index].description) }
+          { name:  `Result ${index}/${totalServers}`, value: 'ㅤ' },
+          { name: 'IP', value: server.ip },
+          { name: 'Port', value: String(server.port) },
+          { name: 'Version', value: getVersion(server.version) },
+          { name: 'Description', value: getDescription(server.description) }
         )
         .setTimestamp()
 
-      var playersString = `${scannedServers[index].players.online}/${scannedServers[index].players.max}`
-      if (scannedServers[index].players.sample != null) {
-        for (var i = 0; i < scannedServers[index].players.sample.length; i++) {
-          playersString += `\n${scannedServers[index].players.sample[i].name}\n${scannedServers[index].players.sample[i].id}`;
-          if (i + 1 < scannedServers[index].players.sample.length) playersString += '\n'
+      var playersString = `${server.players.online}/${server.players.max}`
+      if (server.players.sample != null) {
+        for (var i = 0; i < server.players.sample.length; i++) {
+          playersString += `\n${server.players.sample[i].name}\n${server.players.sample[i].id}`;
+          if (i + 1 < server.players.sample.length) playersString += '\n'
         }
       }
 
       newEmbed.addFields(
         { name: 'Players', value: playersString },
-        { name: 'Last Seen', value: `<t:${scannedServers[index].lastSeen}:f>` }
+        { name: 'Last Seen', value: `<t:${server.lastSeen}:f>` }
       )
 
     interaction.editReply({ content:'', embeds: [newEmbed] });
