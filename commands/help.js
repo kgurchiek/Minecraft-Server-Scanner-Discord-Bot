cf1f56c39ac4cb4603890e0cfa11f66bd3d47cff
kgurchiek <kgurchiek1@gmail.com>
2023-05-16 09:47:52 -0700

    fix spacing in /help

--- commands/help.js
+++ commands/help.js
@@ -29,7 +29,7 @@ module.exports = {
           { name: 'description (regex)', value: 'The description of the server', inline: true },
           { name: 'player (player name)', value: 'The name of a player to search for\n' + '​', inline: true },
           { name: 'seenafter (unix timestamp)', value: `The oldest time a sever can be last seen. This doesn't mean the server is offline, it could be that the ping was lost due to packet loss. Recommended: ${Math.round(new Date().getTime() / 1000) - 3600} (1 hour ago)\n` + '​', inline: true },
-					{ name: 'iprange (ip subnet)', value: 'The ip subnet a server\'s ip has to be within', inline: true },
+					{ name: 'iprange (ip subnet)', value: 'The ip subnet a server\'s ip has to be within\n' + '​', inline: true },
 				{ name: '/streamsnipe', value: 'Gets servers that live Twitch streamers are playing on' }
 		)
     interaction.reply({ embeds: [exampleEmbed], ephemeral:true });
