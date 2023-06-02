4e4683fc6490780ab549aaedba6d4ccfc29eddc3
kgurchiek <kgurchiek1@gmail.com>
2023-06-02 11:44:03 -0700

    Author in /stats

--- commands/stats.js
+++ commands/stats.js
@@ -12,6 +12,7 @@ module.exports = {
     // Status message
     await interaction.reply({ content: 'Retrieving stats...', ephemeral: true });
 		
+    const userTag = await client.users.fetch("720658048611516559");
     const totalServers = await scannedServersDB.countDocuments();
 		var totalSeconds = (interaction.client.uptime / 1000);
     const days = Math.floor(totalSeconds / 86400);
@@ -26,6 +27,7 @@ module.exports = {
       .setAuthor({ name: 'MC Server Scanner', iconURL: 'https://cdn.discordapp.com/app-icons/1037250630475059211/21d5f60c4d2568eb3af4f7aec3dbdde5.png'})
         // Adds more fields to the embed
       .addFields(
+        { name: 'Author:', value: userTag.tag },
         { name: 'Total Servers:', value: String(totalServers) },
 	      { name: 'Bot Stats:', value: `In ${client.guilds.cache.size} Discord servers. Uptime: ${days} days and ${hours}:${minutes}:${seconds}.`}
 	    )
