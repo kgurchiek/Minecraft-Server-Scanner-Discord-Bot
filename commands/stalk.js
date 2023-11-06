0c8872f34dd676e483ffc59168ca6f6bcf6cfbe1
Cornbread2100 <cornbread2100@cornbread2100.com>
2023-11-06 13:06:42 -0500

    Finish /stalk

--- commands/stalk.js
+++ commands/stalk.js
@@ -4,17 +4,79 @@ const { getDescription, getVersion, POST } = require('../commonFunctions.js');
 const config = require('../config.json');
 const languages = require('../languages.json');
 const fs = require('fs');
-const client = require('../index.js');
+const { client } = require('../index.js');
+const maxmind = require('maxmind');
+var cityLookup;
+var asnLookup;
+(async () => {
+  cityLookup = await maxmind.open('./GeoLite2-City.mmdb');
+  asnLookup = await maxmind.open('./GeoLite2-ASN.mmdb');
+})();
 
-async function embed(ip, port, ping, message) {
+async function embed(ip, port, response, message) {
   const oldPlayersID = `oldPlayers${message.id}`;
-  if (ping == 'timeout') {
-    var errorEmbed = new EmbedBuilder()
-      .setColor('#ff0000')
-      .addFields({ name: 'Error', value: 'Timeout (is the server offline?)' })
-    message.edit({ embeds: [errorEmbed] })
-  } else {
-    response = JSON.parse(ping);
+  const oldPlayersFilter = interaction => interaction.customId == oldPlayersID;
+  const oldPlayersCollector = message.channel.createMessageComponentCollector({ filter: oldPlayersFilter });
+  var newEmbed = new EmbedBuilder()
+    .setColor('#02a337')
+    .setTitle('Ping Result')
+    .setAuthor({ name: 'MC Server Scanner', iconURL: 'https://cdn.discordapp.com/app-icons/1037250630475059211/21d5f60c4d2568eb3af4f7aec3dbdde5.png'})
+    .setThumbnail(`https://ping.cornbread2100.com/favicon/?ip=${ip}&port=${port}`)
+    .addFields(
+      { name: 'IP', value: ip },
+      { name: 'Port', value: port.toString() },
+      { name: 'Version', value: getVersion(response.version) + ` (${response.version.protocol})` },
+      { name: 'Description', value: getDescription(response.description) }
+    )
+    .setTimestamp()
+
+  var playersString = `${response.players.online}/${response.players.max}`;
+  if (response.players.sample != null && Array.isArray(response.players.sample)) {
+    var oldString;
+    for (var i = 0; i < response.players.sample.length; i++) {
+      oldString = playersString;
+      playersString += `\n${response.players.sample[i].name}\n${response.players.sample[i].id}`;
+      if (i + 1 < response.players.sample.length) playersString += '\n';
+      if (playersString.length > 1024) {
+        playersString = oldString;
+        break;
+      }
+    }
+  }
+  newEmbed.addFields({ name: 'Players', value: playersString })
+  await message.edit({ embeds: [newEmbed] });
+
+  var location = await cityLookup.get(ip);
+  newEmbed.addFields({ name: 'Country: ', value: location == null ? 'Unknown' : location.country == null ? `:flag_${location.registered_country.iso_code.toLowerCase()}: ${location.registered_country.names.en}` : `:flag_${location.country.iso_code.toLowerCase()}: ${location.country.names.en}` })
+  var org = await asnLookup.get(ip);
+  newEmbed.addFields({ name: 'Organization: ', value: org == null ? 'Unknown' : org.autonomous_system_organization });
+
+  const auth = await (await fetch(`https://ping.cornbread2100.com/cracked/?ip=${ip}&port=${port}&protocol=${response.version.protocol}`)).text();
+  newEmbed.addFields({ name: 'Auth', value: auth == 'true' ? 'Cracked' : auth == 'false' ? 'Premium' : 'Unknown' })
+  await message.edit({ embeds: [newEmbed] });
+
+  const document = await POST('https://api.cornbread2100.com/servers?limit=1', { ip: ip, port: port })
+  if (document.length > 0 && document[0].players.sample != null) {
+    var buttons = new ActionRowBuilder()
+      .addComponents(
+        new ButtonBuilder()
+          .setCustomId(oldPlayersID)
+          .setLabel('Show Old Players')
+          .setStyle(ButtonStyle.Primary)
+      );
+    await message.edit({ components: [buttons] });
+  }
+
+  oldPlayersCollector.on('collect', async interaction => {
+    var buttons = new ActionRowBuilder()
+      .addComponents(
+        new ButtonBuilder()
+          .setCustomId(oldPlayersID)
+          .setLabel('Show Old Players')
+          .setStyle(ButtonStyle.Secondary)
+          .setDisabled(true)
+      );
+
     var newEmbed = new EmbedBuilder()
       .setColor('#02a337')
       .setTitle('Ping Result')
@@ -28,21 +90,35 @@ async function embed(ip, port, ping, message) {
       )
       .setTimestamp()
 
+    const oldPlayers = document[0].players.sample == null ? [] : document[0].players.sample;
+    for (var i = oldPlayers.length - 1; i >= 0; i--) {
+      const player = oldPlayers[i];
+      if (response.players.sample != null && Array.isArray(response.players.sample) && response.players.sample.some(p => p.name == player.name && p.id == player.id)) oldPlayers.splice(i, 1);
+    }
+
     var playersString = `${response.players.online}/${response.players.max}`;
+    var oldString;
     if (response.players.sample != null && Array.isArray(response.players.sample)) {
-      var oldString;
-      for (var i = 0; i < response.players.sample.length; i++) {
+      for (const player of response.players.sample) {
         oldString = playersString;
-        playersString += `\n${response.players.sample[i].name}\n${response.players.sample[i].id}`;
-        if (i + 1 < response.players.sample.length) playersString += '\n';
-        if (playersString.length > 1024) {
-          playersString = oldString;
+        playersString += `\n${player.name} \`online\``;
+        if (playersString.length > 1020 && i + 1 < response.players.sample.length || playersString.length > 1024) {
+          playersString = oldString + '\n...';
           break;
         }
       }
     }
+
+    for (const player of oldPlayers) {
+      oldString = playersString;
+      playersString += `\n${player.name} <t:${player.lastSeen}:R>`;
+      if (playersString.length > 1020 && i + 1 < response.players.sample.length || playersString.length > 1024) {
+        playersString = oldString + '\n...';
+        break;
+      }
+    }
+
     newEmbed.addFields({ name: 'Players', value: playersString })
-    await message.edit({ embeds: [newEmbed] });
 
     var location = await cityLookup.get(ip);
     newEmbed.addFields({ name: 'Country: ', value: location == null ? 'Unknown' : location.country == null ? `:flag_${location.registered_country.iso_code.toLowerCase()}: ${location.registered_country.names.en}` : `:flag_${location.country.iso_code.toLowerCase()}: ${location.country.names.en}` })
@@ -51,83 +127,8 @@ async function embed(ip, port, ping, message) {
 
     const auth = await (await fetch(`https://ping.cornbread2100.com/cracked/?ip=${ip}&port=${port}&protocol=${response.version.protocol}`)).text();
     newEmbed.addFields({ name: 'Auth', value: auth == 'true' ? 'Cracked' : auth == 'false' ? 'Premium' : 'Unknown' })
-    await message.edit({ embeds: [newEmbed] });
-
-    const document = await POST('https://api.cornbread2100.com/servers?limit=1', { ip: ip, port: port })
-    if (document.length > 0 && document[0].players.sample != null) {
-      var buttons = new ActionRowBuilder()
-        .addComponents(
-          new ButtonBuilder()
-            .setCustomId(oldPlayersID)
-            .setLabel('Show Old Players')
-            .setStyle(ButtonStyle.Primary)
-        );
-      await message.edit({ components: [buttons] });
-    }
-
-    oldPlayersCollector.on('collect', async interaction => {
-      var buttons = new ActionRowBuilder()
-        .addComponents(
-          new ButtonBuilder()
-            .setCustomId(oldPlayersID)
-            .setLabel('Show Old Players')
-            .setStyle(ButtonStyle.Secondary)
-            .setDisabled(true)
-        );
-
-      var newEmbed = new EmbedBuilder()
-        .setColor('#02a337')
-        .setTitle('Ping Result')
-        .setAuthor({ name: 'MC Server Scanner', iconURL: 'https://cdn.discordapp.com/app-icons/1037250630475059211/21d5f60c4d2568eb3af4f7aec3dbdde5.png'})
-        .setThumbnail(`https://ping.cornbread2100.com/favicon/?ip=${ip}&port=${port}`)
-        .addFields(
-          { name: 'IP', value: ip },
-          { name: 'Port', value: port.toString() },
-          { name: 'Version', value: getVersion(response.version) + ` (${response.version.protocol})` },
-          { name: 'Description', value: getDescription(response.description) }
-        )
-        .setTimestamp()
-
-      const oldPlayers = document[0].players.sample == null ? [] : document[0].players.sample;
-      for (var i = oldPlayers.length - 1; i >= 0; i--) {
-        const player = oldPlayers[i];
-        if (response.players.sample != null && Array.isArray(response.players.sample) && response.players.sample.some(p => p.name == player.name && p.id == player.id)) oldPlayers.splice(i, 1);
-      }
-
-      var playersString = `${response.players.online}/${response.players.max}`;
-      var oldString;
-      if (response.players.sample != null && Array.isArray(response.players.sample)) {
-        for (const player of response.players.sample) {
-          oldString = playersString;
-          playersString += `\n${player.name} \`online\``;
-          if (playersString.length > 1020 && i + 1 < response.players.sample.length || playersString.length > 1024) {
-            playersString = oldString + '\n...';
-            break;
-          }
-        }
-      }
-
-      for (const player of oldPlayers) {
-        oldString = playersString;
-        playersString += `\n${player.name} <t:${player.lastSeen}:R>`;
-        if (playersString.length > 1020 && i + 1 < response.players.sample.length || playersString.length > 1024) {
-          playersString = oldString + '\n...';
-          break;
-        }
-      }
-
-      newEmbed.addFields({ name: 'Players', value: playersString })
-
-      var location = await cityLookup.get(ip);
-      newEmbed.addFields({ name: 'Country: ', value: location == null ? 'Unknown' : location.country == null ? `:flag_${location.registered_country.iso_code.toLowerCase()}: ${location.registered_country.names.en}` : `:flag_${location.country.iso_code.toLowerCase()}: ${location.country.names.en}` })
-      var org = await asnLookup.get(ip);
-      newEmbed.addFields({ name: 'Organization: ', value: org == null ? 'Unknown' : org.autonomous_system_organization });
-
-      const auth = await (await fetch(`https://ping.cornbread2100.com/cracked/?ip=${ip}&port=${port}&protocol=${response.version.protocol}`)).text();
-      newEmbed.addFields({ name: 'Auth', value: auth == 'true' ? 'Cracked' : auth == 'false' ? 'Premium' : 'Unknown' })
-      await interaction.update({ embeds: [newEmbed], components: [buttons] });
-    })
-  }
+    await interaction.update({ embeds: [newEmbed], components: [buttons] });
+  })
 }
 
 // Exports an object with the parameters for the target server
@@ -185,20 +186,23 @@ const players = {};
 async function stalkCheck() {
   const pingList = getPingList();
   for (const user in pingList) {
+    console.log(user);
     for (const player of pingList[user]) {
-      if (players[player] == 'timeout') continue;
-      const result = (await POST(`https://api.cornbread2100.com/servers?limit=1`, { 'players.sample': { '$exists': true, "$elemMatch": { "name": player }}}))[0];
-      if (result == null) continue;
-      if (players[player] == null || (result.ip != players[player].ip && parsedPing.port != players[player].port)) {
-        var tries = 0;
-        var ping = 'timeout';
-        while (tries < 3 && ping == 'timeout') ping = await (await fetch(`https://ping.cornbread2100.com/ping/?ip=${result.ip}&port=${result.port}`)).text();
-        players[player] = ping == 'timeout' ? 'timeout' : JSON.parse(ping);
-        embed(result.ip, result.port, ping, await client.users.cache.get(user).send({ content: `<@${user}>, ${player} is online.` }));
+      console.log(player);
+      if (players[player] == 'inactive') continue;
+      const result = (await POST(`https://api.cornbread2100.com/servers?limit=1`, { 'players.sample': { '$exists': true, "$elemMatch": { "name": player }},  'lastSeen': { '$gte': Math.round(new Date().getTime() / 1000) - 600 }}))[0];
+      if (result == null) {
+        players[player] = 'inactive';
+        continue;
+      }
+      if (players[player] == null || (result.ip != players[player].ip && result.port != players[player].port)) {
+        players[player] = result;
+        await client.users.fetch(user)
+        embed(result.ip, result.port, result, await client.users.cache.get(user).send({ content: `<@${user}>, ${player} is online.` }));
       }
     }
   }
-  if (Object.keys(players).length > 0) for (const player of players) if (players[player] == 'timeout') players[player] = null;
+  if (Object.keys(players).length > 0) for (const player in players) if (players[player] == 'inactive') players[player] = null;
 }
 stalkCheck();
 setInterval(stalkCheck, 600000); // every 10 minutes
