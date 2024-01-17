9e0fbc689fff7610c2a856b2e71fc515e49bf9a0
kgurchiek <cornbread2100@cornbread2100.com>
2024-01-17 09:47:53 -0500

    use fetch instead of axios

--- commonFunctions.js
+++ commonFunctions.js
@@ -1,5 +1,3 @@
-const axios = require('axios');
-
 function minecraftToAnsi(text) {
   colors = {
     '0': 30,
@@ -130,17 +128,8 @@ function getVersion(rawVersion) {
   return version;
 }
 
-function POST(url, body) {
-  return new Promise((resolve) => {
-    axios.post(url, body)
-      .then((response) => {
-        resolve(response.data)
-      })
-      .catch((error) => {
-        resolve({ error })
-        console.log('POST Error:', error);
-      });
-  })
+async function POST(url, body) {
+  return JSON.parse(await (await fetch(url, { method: 'POST', body: JSON.stringify(body) })).text());
 }
 
 module.exports = {
