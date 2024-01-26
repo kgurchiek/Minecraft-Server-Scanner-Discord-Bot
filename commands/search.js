05d8caa0b58ab2363e47d53dc15850449a5a9d30
Cornbread2100 <cornbread2100@cornbread2100.com>
2024-01-26 11:56:41 -0500

    fix player history list

--- commands/search.js
+++ commands/search.js
@@ -250,8 +250,8 @@ module.exports = {
         updateButtons();
         newEmbed = createEmbed(server, currentEmbed, totalResults);
         if (showingOldPlayers) {
-          var playersString = `${server.players.online}/${server.players.max}\n`;
-          for (const player in server.players.history) playersString += `\`${player.replace(':', ' ')}\` <t:${server.players.history[player]}:${(new Date().getTime() / 1000) - server.players.history[player] > 86400 ? 'D' : 'R'}>`;
+          var playersString = `${server.players.online}/${server.players.max}`;
+          for (const player in server.players.history) playersString += `\n\`${player.replace(':', ' ')}\` <t:${server.players.history[player]}:${(new Date().getTime() / 1000) - server.players.history[player] > 86400 ? 'D' : 'R'}>`;
           newEmbed.data.fields[5].value = playersString;
         }
         await interaction.editReply({ content: '', embeds: [newEmbed], components: [buttons] });
