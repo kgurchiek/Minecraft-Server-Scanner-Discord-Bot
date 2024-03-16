f42331c67531d804d8e91f0ef794993e7d25a00a
Cornbread2100 <cornbread2100@cornbread2100.com>
2024-03-16 17:46:53 -0400

    typo fix

--- commands/info.js
+++ commands/info.js
@@ -11,7 +11,7 @@ module.exports = {
     	.setAuthor({ name: 'MC Server Scanner', iconURL: 'https://cdn.discordapp.com/app-icons/1037250630475059211/21d5f60c4d2568eb3af4f7aec3dbdde5.png'/*, url: 'https://discord.js.org' */})
     	.addFields(
 			{ name: 'Author', value: 'Cornbread2100' + '​' },
-			{ name: 'Source Code', value: 'https://github.com/kgurchiek/Minecraft-Server-Scanner\nhttps://github.com/kgurchiek/Minecraft-Server-Rescanner/\nDiscord bot code: https://github.com/kgurchiek/Minecraft-Server-Scanner-Discord-Bot\nhttps://github.com/kgurchiek/Minecraft-Ping-API/' + '​' },
+			{ name: 'Source Code', value: 'https://github.com/kgurchiek/Minecraft-Server-Scanner\nhttps://github.com/kgurchiek/Minecraft-Server-Rescanner/\nhttps://github.com/kgurchiek/Minecraft-Server-Scanner-Discord-Bot\nhttps://github.com/kgurchiek/Minecraft-Ping-API/' + '​' },
 			{ name: 'Sponsors', value: 'Solia Cloud\nUltraKill Hosting' },
 		)
     interaction.reply({ embeds: [newEmbed], ephemeral: true });
