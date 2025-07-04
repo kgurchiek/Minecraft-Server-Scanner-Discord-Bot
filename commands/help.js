2cf0eaa73aed96e3e43aba5386d6a1d81027c801
okcoder1 <codershub.code@gmail.com>
2025-07-04 22:37:45 +0530

    grammar errors

--- commands/help.js
+++ commands/help.js
@@ -17,9 +17,9 @@ module.exports = {
 			new EmbedBuilder()
 				.setColor("#02a337")
 				.setTitle('/ping')
-				.setDescription('Fetches info from a given Minecraft Java server')
+				.setDescription('Fetches info from a given Java Edition server')
 				.addFields(
-					{ name: 'ip', value: 'The ip address of the server', inline: true },
+					{ name: 'ip', value: 'The IP address of the server', inline: true },
 					{ name: 'port', value: 'The port of the server (defaults to 25565)', inline: true }
 				),
 			new EmbedBuilder()
@@ -27,7 +27,7 @@ module.exports = {
 				.setTitle('/bedrockping')
 				.setDescription('Fetches info from a given Minecraft Bedrock server')
 				.addFields(
-					{ name: 'ip', value: 'The ip address of the server', inline: true },
+					{ name: 'ip', value: 'The IP address of the server', inline: true },
 					{ name: 'port', value: 'The port of the server (defaults to 19132)', inline: true }
 				),
 			new EmbedBuilder()
@@ -35,7 +35,7 @@ module.exports = {
 				.setTitle('/search')
 				.setDescription('Searches the database for a Java Edition server with specific properties')
 				.addFields(
-					{ name: 'minimal (true/false)', value: 'Only shows ip and port in preview (recommended for mobile users)', inline: true },
+					{ name: 'minimal (true/false)', value: 'Only shows IP and port in preview (recommended for mobile users)', inline: true },
 					{ name: 'page (integer)', value: 'Skips to a page of results', inline: true },
 					{ name: 'playercount (range)', value: 'A range of how many players on on the server (e.g. 4, >10, <=5, 11-20)', inline: true },
 					{ name: 'playercap (integer)', value: 'The maximum player capacity of the server', inline: true },
@@ -47,10 +47,10 @@ module.exports = {
 					{ name: 'description (text)', value: 'The description of the server', inline: true },
 					{ name: 'hasplayerlist (boolean)', value: 'Whether or not the server has player list enabled', inline: true },
 					{ name: 'seenafter (unix timestamp)', value: `The oldest time a server can be last seen. This doesn't mean the server is offline, it could be that the ping was lost due to packet loss. Recommended: ${Math.round(new Date().getTime() / 1000) - 3600} (1 hour ago)\n` + '​', inline: true },
-					{ name: 'iprange (ip subnet)', value: 'The ip subnet a server\'s ip has to be within', inline: true },
+					{ name: 'iprange (ip subnet)', value: 'The IP subnet a server\'s IP has to be within', inline: true },
 					{ name: 'port (integer)', value: 'The port the server is hosted on', inline: true },
 					{ name: 'country (text)', value: 'The country the server is hosted in (use the autocomplete options)', inline: true },
-					{ name: 'org (text)', value: 'The organization the ip belongs to', inline: true },
+					{ name: 'org (text)', value: 'The organization the IP belongs to', inline: true },
 					{ name: 'cracked (true/false)', value: 'Whether or not the server is cracked (offline mode)', inline: true },
 					{ name: 'whitelist (true/false)', value: 'Whether or not the server has a whitelist', inline: true },
 					{ name: 'vanilla (true/false)', value: 'Whether or not the server is vanilla', inline: true },
@@ -60,7 +60,7 @@ module.exports = {
 				.setTitle('/bedrocksearch')
 				.setDescription('Searches the database for a Bedrock Edition server with specific properties')
 				.addFields(
-					{ name: 'minimal (true/false)', value: 'Only shows ip and port in preview (recommended for mobile users)', inline: true },
+					{ name: 'minimal (true/false)', value: 'Only shows IP and port in preview (recommended for mobile users)', inline: true },
 					{ name: 'page (integer)', value: 'Skips to a page of results', inline: true },
 					{ name: 'playercount (range)', value: 'A range of how many players on on the server (e.g. 4, >10, <=5, 11-20)', inline: true },
 					{ name: 'playercap (integer)', value: 'The maximum player capacity of the server', inline: true },
@@ -68,11 +68,11 @@ module.exports = {
 					{ name: 'version (text)', value: 'The version of the server', inline: true },
 					{ name: 'description (text)', value: 'The description of the server', inline: true },
 					{ name: 'seenafter (unix timestamp)', value: `The oldest time a server can be last seen. This doesn't mean the server is offline, it could be that the ping was lost due to packet loss. Recommended: ${Math.round(new Date().getTime() / 1000) - 3600} (1 hour ago)\n` + '​', inline: true },
-					{ name: 'iprange (ip subnet)', value: 'The ip subnet a server\'s ip has to be within', inline: true },
+					{ name: 'iprange (ip subnet)', value: 'The IP subnet a server\'s IP has to be within', inline: true },
 					{ name: 'port (integer)', value: 'The port the server is hosted on', inline: true },
 					{ name: 'gamemode (text)', value: 'The default game mode of the server', inline: true },
 					{ name: 'country (text)', value: 'The country the server is hosted in (use the autocomplete options)', inline: true },
-					{ name: 'org (text)', value: 'The organization the ip belongs to', inline: true },
+					{ name: 'org (text)', value: 'The organization the IP belongs to', inline: true },
 				),
 			]
     interaction.reply({ embeds, ephemeral: true });
